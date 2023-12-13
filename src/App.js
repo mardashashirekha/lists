d0810e5cd3f4283d4323ee68532f3a360d497f96
mardashashirekha <losha6300@gmail.com>
2023-12-13 16:20:30 +0000

    npm install

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile/index'
+const userDetails={
+    imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png
+",
+    name:"Shashirekha",
+    role:"Software"
+}
+
+const App = () =<UserProfile/>
+
+export default App
---
+++ src/components/UserProfile/index.js
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+  ;<div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/esther-howard-img.png
+"
+    />
+  </div>
+}
+export default UserProfile
